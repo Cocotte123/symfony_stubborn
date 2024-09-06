dbaeeaf2808f5b9f0d68cf28fb0756e3cec48c87
Cocotte123 <rousseauemmanuelle1@yahoo.fr>
2024-09-06 14:37:47 +0200

    initial

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ConferenceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class HomePageController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home_page")
+     */
+    
+    public function index(): Response
+    {
+       return $this->render('home_page/home.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    
+         
+}
