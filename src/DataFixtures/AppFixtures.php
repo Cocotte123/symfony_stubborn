721482125b822408c895d15bb86cd5a6cb2bde00
Cocotte123 <rousseauemmanuelle1@yahoo.fr>
2024-10-15 16:29:35 +0200

    fixtures2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,8 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\SearchPrice;
+use App\Entity\Size;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -26,6 +28,17 @@ class AppFixtures extends Fixture
             ->setDeliveryAdress('Adress')
             ->setIsVerified(true);
         $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('admin@gmail.com')
+            ->setRoles(["ROLE_ADMIN"])
+            //->setPassword($this->$hasher->hashPassword($user,'123456'))
+            //->setPassword($hasher->hashPassword($user,'123456'))
+            ->setPassword('$2y$13$ap3qK83V4AGYp9wAwklEyOyAPCbYnDPK8lhuOf552y4V3gO4Dv7JC')
+            ->setName('admin')
+            ->setDeliveryAdress('Adress')
+            ->setIsVerified(true);
+        $manager->persist($user);
         
         $product = new Product();
         for($i = 0; $i<20;$i++){
@@ -33,7 +46,7 @@ class AppFixtures extends Fixture
             $product->setName("product{$i}")
                     ->setPrice('30')
                     ->setIsHighlighted(false)
-                    ->setImage('image')
+                    ->setImage("product{$i}.jpg")
                     ->setXS('2')
                     ->setS('2')
                     ->setM('2')
@@ -42,6 +55,77 @@ class AppFixtures extends Fixture
             $manager->persist($product);
         }
         // $manager->persist($product);
+
+        $searchPrice = new SearchPrice();
+        $searchPrice->setName('10 à 29€')
+                    ->setMinLimit('10')
+                    ->setMaxLimit('30')
+                    ->setOrderBy('2');
+            
+        $manager->persist($searchPrice);
+        
+
+        $searchPrice = new SearchPrice();
+        $searchPrice->setName('30 à 35€')
+                    ->setMinLimit('30')
+                    ->setMaxLimit('36')
+                    ->setOrderBy('3');
+            
+        $manager->persist($searchPrice);
+
+        $searchPrice = new SearchPrice();
+        $searchPrice->setName('36 à 50€')
+                    ->setMinLimit('36')
+                    ->setMaxLimit('51')
+                    ->setOrderBy('4');
+            
+        $manager->persist($searchPrice);
+
+        $searchPrice = new SearchPrice();
+        $searchPrice->setName(' ')
+                    ->setMinLimit('')
+                    ->setMaxLimit('')
+                    ->setOrderBy('5');
+            
+        $manager->persist($searchPrice);
+
+        $searchPrice = new SearchPrice();
+        $searchPrice->setName('Fourchette de prix')
+                    ->setMinLimit('')
+                    ->setMaxLimit('')
+                    ->setOrderBy('1');
+            
+        $manager->persist($searchPrice);
+
+        $size = new Size();
+        $size->setSize('XS')
+                    ->setSizeOrder('1');
+            
+        $manager->persist($size);
+
+        $size = new Size();
+        $size->setSize('S')
+                    ->setSizeOrder('2');
+            
+        $manager->persist($size);
+
+        $size = new Size();
+        $size->setSize('M')
+                    ->setSizeOrder('3');
+            
+        $manager->persist($size);
+
+        $size = new Size();
+        $size->setSize('L')
+                    ->setSizeOrder('4');
+            
+        $manager->persist($size);
+
+        $size = new Size();
+        $size->setSize('XL')
+                    ->setSizeOrder('5');
+            
+        $manager->persist($size);
 
         $manager->flush();
     }
